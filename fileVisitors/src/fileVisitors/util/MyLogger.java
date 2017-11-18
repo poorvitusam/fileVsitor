c5bdea02d5463ef1bf2edf185991d3ff2a8ff2fb
poorvitusam <poorvitusam@gmail.com>
2017-11-18 00:27:29 -0500

    Added MyLogger

---
+++ fileVisitors/src/fileVisitors/util/MyLogger.java
@@ -0,0 +1,38 @@
+package fileVisitors.util;
+
+public class MyLogger{
+
+	public static enum DebugLevel {RELEASE, DEBUG, VERBOSE, RUNNABLE, DELETED, CONSTRUCTOR};
+
+	private static DebugLevel debugLevel;
+
+
+	public static void setDebugValue (int levelIn) {
+		switch (levelIn) {
+		case 4: debugLevel = DebugLevel.CONSTRUCTOR; break;
+		case 3: debugLevel = DebugLevel.RUNNABLE; break;
+		case 2: debugLevel = DebugLevel.VERBOSE; break;
+		case 1: debugLevel = DebugLevel.DEBUG; break;
+		case 0: debugLevel = DebugLevel.RELEASE; break;
+		}
+	}
+
+	public static void setDebugValue (DebugLevel levelIn) {
+		debugLevel = levelIn;
+	}
+
+	// @return None
+	public static void writeMessage (String  message  ,
+			DebugLevel levelIn ) {
+		if (levelIn == debugLevel)
+			System.out.println(message);
+	}
+
+	/**
+	 * @return String
+	 */
+	public String toString() {
+		return "Debug Level is " + debugLevel;
+	}
+}
+
