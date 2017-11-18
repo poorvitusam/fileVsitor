898638ab120d33855d46dffc797b9ae7ec19b40e
poorvitusam <poorvitusam@gmail.com>
2017-11-18 01:28:13 -0500

    Added tree structure

---
+++ fileVisitors/src/fileVisitors/tree/Node.java
@@ -0,0 +1,122 @@
+package fileVisitors.tree;
+
+import fileVisitors.util.MyLogger;
+import fileVisitors.util.MyLogger.DebugLevel;
+
+/**
+ * Class that represents Node of a tree.
+ * @author suresh
+ *
+ */
+public class Node {
+
+
+	Node leftNode;
+	Node rightNode;
+
+	String word;
+	int occurrence;
+
+	@SuppressWarnings("unused")
+	private Node() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Parameterized constructor to restrict 1 Node to 1 BNumber relation. Also, initializes
+	 * all member variables
+	 * @param bNumber
+	 */
+	public Node(String wordI) {
+		MyLogger.writeMessage("Node Parameterized Constructor is called ", DebugLevel.CONSTRUCTOR);
+		word = wordI;
+		leftNode = null;
+		rightNode = null;
+	}
+
+	/**
+	 * Getter method for Word
+	 * @return
+	 */
+	public String getWord() {
+		return word;
+	}
+
+
+	/**
+	 * Getter method for occurrences of the word
+	 * @return
+	 */
+	public int getOccurrence() {
+		return occurrence;
+	}
+
+	/**
+	 * Increase the word count by one
+	 */
+	public void incrementWordCount()  {
+		occurrence += 1;
+	}
+
+
+	/**
+	 * Decrease the word count by one
+	 */
+	public void decrementWordCount()  {
+		if(occurrence > 0)
+			occurrence -= 1;
+	}
+
+	/**
+	 * Getter Method for Left Node 
+	 * @return Node
+	 */
+	public Node getLeftNode() {
+		return leftNode;
+	}
+
+	/**
+	 * Setter method for LeftNode
+	 * @param leftNode
+	 */
+	public void setLeftNode(Node leftNode) {
+		this.leftNode = leftNode;
+	}
+
+	/**
+	 * Getter Method for Right Node
+	 * @return Node
+	 */
+	public Node getRightNode() {
+		return rightNode;
+	}
+
+	/**
+	 * Setter method for Right Node 
+	 * @param rightNode
+	 */
+	public void setRightNode(Node rightNode) {
+		this.rightNode = rightNode;
+	}
+
+
+
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		Node node = new Node(this.word);
+		node.occurrence = this.getOccurrence();
+		return node;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();	
+
+		builder.append(word);
+		builder.append(" [" + occurrence + "] ");
+
+		return builder.toString();
+	}
+
+
+}
---
+++ fileVisitors/src/fileVisitors/tree/Tree.java
@@ -0,0 +1,170 @@
+package fileVisitors.tree;
+
+import fileVisitors.store.Results;
+import fileVisitors.util.MyLogger;
+import fileVisitors.util.MyLogger.DebugLevel;
+import fileVisitors.visitor.VisitorI;
+
+/**
+ * Helper class that holds and create a Binary Search Tree for given words and count
+ * @author suresh
+ *
+ */
+public class Tree {
+	
+	Node rootNode; 
+	
+	int numberOfDistinctWords;
+	int numberOfTotalWords;
+	int numberOfCharacters;
+	
+	public Tree() {
+		MyLogger.writeMessage("Tree Parameterized Constructor is called ", DebugLevel.CONSTRUCTOR);
+	}
+	
+	public void setRootNode(Node rootNode) {
+		this.rootNode = rootNode;
+	}
+	
+	public void accept(VisitorI visitor) {
+		visitor.visit(this);
+	}
+	
+	/**
+	 * Add new node to tree
+	 * @param node
+	 */
+	public synchronized void addWord(String word) {
+		if(word == null || word.length() == 0) return;
+		
+		if(rootNode == null) {
+			rootNode = new Node(word);
+			rootNode.occurrence = 1;
+			return;
+		}
+		
+		traverseAndAdd(rootNode, word);
+	}
+	
+	/**
+	 * Traverse through the tree and find the appropriate place for new node to store.
+	 * @param root
+	 * @param nodeToAdd
+	 * @return node
+	 */
+	private Node traverseAndAdd(Node root, String wordToAdd) {
+		
+		if(root == null) {
+			root = new Node(wordToAdd);
+			root.occurrence = 1;
+			return root;
+		}
+		
+		if(wordToAdd.compareTo(root.getWord()) > 0) {
+			root.setRightNode(traverseAndAdd(root.getRightNode(), wordToAdd));
+		} else if(wordToAdd.compareTo(root.getWord()) < 0) {
+			root.setLeftNode(traverseAndAdd(root.getLeftNode(), wordToAdd));
+		} else {
+			root.incrementWordCount();
+		}
+		
+		return root;
+	}
+	
+	/**
+	 * Remove course from list or unregister student from given course
+	 * @param bNumber
+	 * @param course
+	 */
+	public synchronized void delete(String word) {
+		Node node = lookup(rootNode, word);
+		
+		if(node != null) {
+			node.decrementWordCount();
+		}
+	}
+	
+	/**
+	 * Lookup node for given bNumber and return
+	 * @param rootNode
+	 * @param bNumber
+	 * @return <b>node</b> if bnumber found in tree, <b>null</b> otherwise
+	 */
+	public Node lookup(Node rootNode, String word) {
+		Node toReturn = null;
+		
+		if (rootNode == null) return null;
+		
+		if(rootNode.getWord().compareTo(word) > 0) {
+			toReturn = lookup(rootNode.leftNode, word);
+		} else if(rootNode.getWord().compareTo(word) < 0) {
+			toReturn = lookup(rootNode.rightNode, word);
+		} else {
+			toReturn = rootNode;
+		}
+		
+		return toReturn;
+	}
+	
+	/**
+	 * Store and write results to file in format 1234:A B C
+	 * @param results
+	 */
+	public void printNodes(Results results) {
+		printInAscendingOrder(rootNode, results);
+		results.writeToFile();
+	}
+	
+	/**
+	 * Traverse tree in In Order form and store result in Results object
+	 * @param node
+	 * @param result
+	 */
+	private void printInAscendingOrder(Node node, Results result) {
+		if(node == null) return;
+		
+		printInAscendingOrder(node.getLeftNode(), result);
+		
+		result.storeNewResult(node.toString());
+		
+		printInAscendingOrder(node.getRightNode(), result);
+	}
+	
+	/**
+	 * Traverse tree in In Order form and store result in Results object
+	 * @param node
+	 * @param result
+	 */
+	private void calculateCount(Node node) {
+		if(node == null) return;
+		
+		calculateCount(node.getLeftNode());
+		
+		if(node.occurrence > 0) {
+			numberOfCharacters += node.word.length() * node.occurrence;
+			numberOfDistinctWords += 1;
+			numberOfTotalWords += node.occurrence;
+		}
+		
+		calculateCount(node.getRightNode());
+	}
+	
+	public void calculateStats() {
+		calculateCount(rootNode);
+	}
+	
+	public int getNumberOfCharacters() {
+		return numberOfCharacters;
+	}
+	
+	
+	public int getNumberOfTotalWords() {
+		return numberOfTotalWords;
+	}
+
+	
+	public int getNumberOfDistinctWords() {
+		return numberOfDistinctWords;
+	}
+}
+
