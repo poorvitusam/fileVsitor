4e53b8940740610b1660597770aeba58f9e76e63
Suresh <slalcha1@binghamton.edu>
2017-11-18 18:31:45 -0500

    Implemented Logic to check if the length of string is prime and append the word with -PRIME suffix

--- fileVisitors/src/fileVisitors/visitor/PrimeLength.java
+++ fileVisitors/src/fileVisitors/visitor/PrimeLength.java
@@ -1,10 +1,48 @@
 package fileVisitors.visitor;
 
+import fileVisitors.tree.Node;
 import fileVisitors.tree.Tree;
 
 public class PrimeLength implements VisitorI{
 
 	public void visit(Tree tree) {
+		processTree(tree.getRootNode());
+	}
+
+	private void processTree(Node node) {
+
+		if(node == null) return;
+
+		processTree(node.getLeftNode());
+
+		if(isPrimeLength(node.getWord())) {
+			String word = node.getWord() + "-PRIME";
+			node.setWord(word);
+
+		}
+
+		processTree(node.getRightNode());
 
 	}
+
+	private boolean isPrimeLength(String str) {
+
+		int length = str.length();
+
+		if(length <= 1) return false;
+
+		for (int i = 2; i < length; i++) {
+			if (length % i == 0) {
+				return false;
+			}
+		}
+
+		return true;
+
+	}
+
+
+
+
+
 }
